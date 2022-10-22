4f12e9cca2f55a528d081ab6adfa79a0883828fc
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2022-10-22 22:20:22 +0530

    Added form handling and form submission logic for signup and signin

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Auth/SignIn.css";
 import signin from "../../assets/images/sign_in.png";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    if (/\S+@\S+\.\S+/.test(e.target.value))
+      setErrors({ email: null, password: null });
+    else setErrors({ email: "Invalid Email", password: null });
+  };
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+    if (e.target.value.length > 5) setErrors({ email: null, password: null });
+    else
+      setErrors({
+        email: null,
+        password: "Password must be atleast 6 characters long",
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errors) alert("Form Has Errors!");
+    else {
+      fetch(
+        "https://dkgicggupnrxldwvkeft.supabase.co/auth/v1/token?grant_type=password",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            apikey:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZ2ljZ2d1cG5yeGxkd3ZrZWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYwMDI4ODMsImV4cCI6MTk4MTU3ODg4M30.BLLinQ9VEK8_T-JE22WOidlJs_0TFhOb1n3zkSVc7eg",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      ).then((response) => {
+        response.json().then((result) => console.log(result));
+      });
+    }
+  };
+
   return (
     <div className="signin-container">
       <div className="header">
@@ -12,7 +62,7 @@ export default function SignIn() {
       </div>
       <div className="auth-form-container">
         <p className="header">Sign In</p>
-        <form className="signin-form">
+        <form className="signin-form" onSubmit={handleSubmit}>
           <img className="signin-image" src={signin} alt="Sign In Image" />
           <label htmlFor="email">Email: </label>
           <input
@@ -20,18 +70,25 @@ export default function SignIn() {
             id="email"
             name="email"
             autoComplete="off"
+            value={email}
+            onChange={handleEmailChange}
             required
           />
-
+          <span style={{ color: "red" }}>{errors["email"]}</span>
           <label htmlFor="password">Password: </label>
           <input
             type="password"
             id="password"
             name="password"
             autoComplete="off"
+            value={password}
+            onChange={handlePasswordChange}
             required
           />
-          <button className="submit-button">Sign In</button>
+          <span style={{ color: "red" }}>{errors["password"]}</span>
+          <button className="submit-button" type="submit">
+            Sign In
+          </button>
         </form>
       </div>
     </div>
--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Auth/SignUp.css";
 import signup from "../../assets/images/sign_up.png";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    if (/\S+@\S+\.\S+/.test(e.target.value)) {
+      setErrors({ email: null, password: null });
+    } else setErrors({ email: "Invalid Email", password: null });
+  };
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+    if (e.target.value.length > 5) setErrors({ email: null, password: null });
+    else
+      setErrors({
+        email: null,
+        password: "Password must be atleast 6 characters long",
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(password);
+    if (!errors) alert("Form Has Errors");
+    else {
+      fetch("https://dkgicggupnrxldwvkeft.supabase.co/auth/v1/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          apikey:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZ2ljZ2d1cG5yeGxkd3ZrZWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYwMDI4ODMsImV4cCI6MTk4MTU3ODg4M30.BLLinQ9VEK8_T-JE22WOidlJs_0TFhOb1n3zkSVc7eg",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          data: {
+            name: name,
+          },
+        }),
+      }).then((response) => {
+        response.json().then((result) => console.log(result));
+      });
+    }
+  };
+
   return (
     <div className="signup-container">
       <div className="header">
@@ -11,26 +70,41 @@ export default function SignUp() {
         </p>
       </div>
       <div className="auth-form-container">
-        <p className="header">Sign Un</p>
-        <form className="signup-form">
+        <p className="header">Sign Up</p>
+        <form className="signup-form" onSubmit={handleSubmit}>
           <img className="signup-image" src={signup} alt="Sign Up Image" />
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
           <label htmlFor="email">Email: </label>
           <input
             type="text"
             id="email"
             name="email"
             autoComplete="off"
+            value={email}
+            onChange={handleEmailChange}
             required
           />
-
+          <span style={{ color: "red" }}>{errors["email"]}</span>
           <label htmlFor="password">Password: </label>
           <input
             type="password"
             id="password"
             name="password"
             autoComplete="off"
+            value={password}
+            onChange={handlePasswordChange}
             required
           />
+          <span style={{ color: "red" }}>{errors["password"]}</span>
           <button className="submit-button">Sign Up</button>
         </form>
       </div>
